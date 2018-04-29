50d334f83f0f12de2adf18fd4045b9dc9dc82182
Ubuntu <ubuntu@ip-172-31-95-141.ec2.internal>
2018-04-29 22:47:14 +0000

    PCA implementation

--- pca_mnist.py
+++ pca_mnist.py
@@ -5,33 +5,33 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import matplotlib
 
-from time import time
+import time
 from sklearn.decomposition import PCA, KernelPCA
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
 from svm_classify import * 
+from sklearn.datasets import fetch_mldata
 
 import math
 
-train_mnist = pd.read_csv('train.csv')
-xtrain = train_mnist.drop(['label'],axis='columns',inplace=False)
-ytrain = train_mnist['label']
+mnist = fetch_mldata("MNIST original")
+xtrain = mnist.data / 255.0
+ytrain = mnist.target
 
 # X_tr, X_ts, Y_tr, Y_ts = train_test_split(xtrain,ytrain,test_size=0.30,random_state=4)
 
 n_components = 16
-t0 = time()
+time_start = time.time()
 pca = PCA(n_components = n_components, svd_solver = 'randomized',whiten = True).fit(xtrain)
-kpca = KernelPCA(n_components = n_components, kernel = 'rbf',fit_inverse_transform = True ,gamma = 10).fit(xtrain)
-
-print("done in %0.3fs" % (time()-t0))
+#kpca = KernelPCA(n_components = n_components, kernel = 'rbf',fit_inverse_transform = True ,gamma = 10).fit(xtrain)
+time_end = time.time()
+print("done in %0.3fs" % (time.time() - time_start))
 pca.explained_variance_ratio_.sum() 
 
 xtrain_pca = pca.transform(xtrain)
 xtrain_kpca = kpca.transform(xtrain)
 
-t1 = time()
 svm_classify(xtrain_pca,ytrain)
 # param = {"C" : [0.1], "gamma":[0.1]}
 # rf = SVC()
@@ -46,7 +46,7 @@ svm_classify(xtrain_pca,ytrain)
 # print(gs_pca.best_score_)
 # print(gs_pca.best_params_)
 
-# bp_pca = gs_pca.best_params_
+# bp_pca = gs_pca.best_fpfarams_
 # # bp = gs.best_params_
 
 # # t0 = time()
